f116aa469b78cbc7b91efb7ba499385cd35d9767
Simon Sørensen <fynboen1999@gmail.com>
2018-12-17 22:48:07 +0100

    Created config.py
    Email details

---
+++ config.py
@@ -0,0 +1,9 @@
+details = {
+	'name': 'Simon S.',
+	'portal_url': 'https://mail.google.com',
+	'email_address': 'your_email_address',
+	'email_password': 'your_email_password',
+	'subject': 'Python browser automation',
+	'message': 'Looking to automate your own mailing list? Go download this coding example at - https://github.com/SnaBe/auto-email \n - Your name,
+	'users': ['user_email_1', 'user_email_2']
+}
